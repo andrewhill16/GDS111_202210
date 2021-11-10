38c76d3b307aa652d31654fc641d30c0bdc414a6
Hill <ahill2@email.neit.edu>
2021-11-10 17:34:15 -0500

    end of w6d2 class

--- rps_start-1/rps_start/rps.js
+++ rps_start-1/rps_start/rps.js
@@ -17,6 +17,19 @@ pics2[0] = "images/rock2.jpg"
 pics2[1] = "images/paper2.jpg"
 pics2[2] = "images/scissors2.jpg"
 
+//array of buttons
+//query selector grabs all of one element type
+var btn = document.querySelectorAll("button")
+
+//check stored data in the console
+console.log(btn) //used for testing, requires dev tools to be open
+
+//make button clickable and also run the game
+//add event listeners to each button
+btn[0].addEventListener("click", function(e) {play(0)})
+btn[1].addEventListener("click", function(e) {play(1)})
+btn[2].addEventListener("click", function(e) {play(2)})
+
 //arrays that store player and computer opitons
 //player id = pId
 var pId = new Array("rock_p", "paper_p", "scissors_p")
@@ -33,6 +46,8 @@ function swap(id, image){
 //play function
 function play(id){
     //setting up stored image paths in js to match the HTML ones
+    //swap calls the function and gets it code to run
+    //values supplied inside are passed into the parameter value
     swap(pId[0], pics[0])
     swap(pId[1], pics[1])
     swap(pId[2], pics[2])
@@ -58,38 +73,46 @@ function play(id){
         //rock
         case 0:
         if (c_choice == 0){
-            alert("Bloody hell it's a draw")
+            //call showResults and pass correct value for pChoice cChoice and results
+            showResults("Rock", "Rock", "Draw")
         }
         else if (c_choice == 1){
-            alert("You lost to a computer my gamer")
+            showResults("Rock", "Paper", "Point for Skynet")
         }
         else{
-            alert("You win pog")
+            showResults("Rock", "Scissors", "John Connor Would be Proud")
         }
         //break statements break out of the switch/case
         break
         //paper
         case 1:
         if (c_choice == 1){
-            alert("Bloody hell it's a draw")
+            showResults("Paper", "Paper", "Draw")
         }
         else if (c_choice == 2){
-            alert("You lost to a computer my gamer")
+            showResults("Paper", "Scissors", "Point for Skynet")
         }
         else{
-            alert("You win pog")
+            showResults("Paper", "Rock", "John Connor Would be Proud")
         }
         break
         //scissors
         case 2:
         if (c_choice == 2){
-            alert("Bloody hell it's a draw")
+            showResults("Scissors", "Scissors", "Draw")
         }
         else if (c_choice == 0){
-            alert("You lost to a computer my gamer")
+            showResults("Scissors", "Rock", "Point for Skynet")
         }
         else{
-            alert("You win pog")
+            showResults("Scissors", "Paper", "John Connor Would be Proud")
         }
-    }
+    }//end switch statement
+}//end play function
+
+//function that writes reslts back to the HTML page
+function showResults(pChoice, cChoice, results){
+    document.getElementById("pChoice").innerHTML = pChoice
+    document.getElementById("cChoice").innerHTML = cChoice
+    document.getElementById("results").innerHTML = results
 }
